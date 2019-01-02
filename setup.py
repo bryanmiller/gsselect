43f1ed7a2382de1544b20bd102d1cb8f52a2a2ae
Bryan Miller <millerwbryan@gmail.com>
2019-01-02 14:13:36 -0300

    Minor format updates

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='gsselect',
-     version='1.0.1',
-     scripts=['gsselect.py', 'gemcats.py', 'inpoly.py', 'parangle.py', 'urltrigger.py'] ,
-     author="Bryan Miller",
-     author_email="millerwbryan@gmail.com",
-     description="Gemini guide star selection and URL TOO triggering",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/bryanmiller/gsselect",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python",
-         'Intended Audience :: Science/Research',
-         "License :: AURA copyright",
-         "Operating System :: OS Independent",
-     ],
-     install_requires=[
-         'astropy',
-         'matplotlib',
-         'numpy',
-         'aplpy'
-     ]
+    name='gsselect',
+    version='1.0.1',
+    scripts=['gsselect.py', 'gemcats.py', 'inpoly.py', 'parangle.py', 'urltrigger.py'],
+    author="Bryan Miller",
+    author_email="millerwbryan@gmail.com",
+    description="Gemini guide star selection and URL TOO triggering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bryanmiller/gsselect",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Science/Research",
+        "License :: AURA copyright",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'astropy',
+        'matplotlib',
+        'numpy',
+        'aplpy'
+    ]
 )
