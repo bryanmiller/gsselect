3cd6edb2711926bc8a414a81ab98e1eb1df1314f
Bryan Miller <millerwbryan@gmail.com>
2019-03-06 11:03:03 -0300

    Push to version 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='gsselect',
-    version='1.1.0',
+    version='1.1.1',
     author="Bryan Miller",
     author_email="millerwbryan@gmail.com",
     description="Gemini guide star selection and URL TOO triggering",
